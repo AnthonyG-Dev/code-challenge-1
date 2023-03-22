ba1a376790a12d564d4657c9d7088efd380a6757
GicheruAnthony <giterugicheru@gmail.com>
2023-03-22 11:28:32 +0300

    challenge 3 update

--- index.js
+++ index.js
@@ -1,62 +1,17 @@
-//code challenge 1
-function checkGrade(marks) {
-  const output = document.getElementById("grades");
+const TAX_RATE = 0.25;
+const NHIF_RATE = 0.02;
+const NSSF_RATE = 0.12;
 
-  if (marks >= 80 && marks <= 100) {
-    console.log("Grade: A");
-    output.value = "Grade A";
-  } else if (marks >= 60 && marks <= 79) { 
-    console.log("Grade: B");
-    output.value = "Grade B";
-  } else if (marks >= 50 && marks <= 59) {
-    console.log("Grade: C");
-    output.value = "Grade C";
-  } else if (marks >= 40 && marks <= 49) {
-    console.log("Grade: D");
-    output.value = "Grade D";
-  } else if (marks < 40 && marks >= 0) {
-    console.log("Grade: E");
-    output.value = "Grade E";
-  } else {
-    console.log("Invalid input! Please enter marks between 0 and 100.");
-  }
+function calculateSalary(basicSalary, benefits) {
+  const grossSalary = basicSalary + benefits;
+  const taxDeduction = grossSalary * TAX_RATE;
+  const nhifDeduction = grossSalary * NHIF_RATE;
+  const nssfDeduction = grossSalary * NSSF_RATE;
+  const netSalary = grossSalary - taxDeduction - nhifDeduction - nssfDeduction;
+
+  console.log(`Gross Salary: ${grossSalary}`);
+  console.log(`PAYE (Tax): ${taxDeduction}`);
+  console.log(`NHIF Deduction: ${nhifDeduction}`);
+  console.log(`NSSF Deduction: ${nssfDeduction}`);
+  console.log(`Net Salary: ${netSalary}`);
 }
-//button functionality
-function buttonClicked() {
-  const value = document.getElementById("marks").value;
-  console.log(value);
-  checkGrade(value);
-}
-//code challenge 2
-
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const pointsPerDemerit = 5;
-  const demeritLimit = 12;
-  const output = document.getElementById("speedOutput");
-
-  if (speed <= speedLimit) {
-    console.log("Ok");
-    output.value = "Ok";
-  }else {
-    const demeritPoints = Math.floor((speed - 70) / 5);
-    if (demeritPoints > 12) {
-      console.log("License suspended");
-      output.value = "License suspended";
-    } else {
-      console.log(`Points: ${demeritPoints}`);
-      output.value = `Points: ${demeritPoints}`;
-    }
-  }
-}
-checkSpeed(70)
-
-//button functionality
-
-  function speedButtonClicked() {
-  const value = document.getElementById("speed").value;
-  console.log(value);
-  checkSpeed(value);
-}
-
-// challenge 3
